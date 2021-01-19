fc15b1f693435e3fcff7dea599082430588eb284
Silvia Gutierrez Balaguer <silviagutierrezbalaguer@MBP-de-Silvia.JOOWIN>
2021-01-19 10:02:06 +0100

    vue styles

--- js/vue.js
+++ js/vue.js
@@ -2,23 +2,11 @@ const app = Vue.createApp({
     data(){
         return{
             counter: 0,
-            textStyle:{
-                color:""
-            }
         }
     },
     methods:{
         updateCounter(valor){
             this.counter += valor
-            if(this.counter < 0){
-                this.textStyle.color="red"
-            }
-            else if(this.counter === 0){
-                this.textStyle.color="#333333"
-            }
-            else{
-                this.textStyle.color="green"
-            }
         }
     }
 })
